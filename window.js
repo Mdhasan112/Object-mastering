f26d03c813a1a432a985870d506ff348a0090568
hasan <mdhasanhk.98@gmail.com>
2022-07-28 16:46:22 -0400

    window

---
+++ window.js
@@ -0,0 +1,14 @@
+const name = "kuddus"
+function add(num1, num2) {
+    const result = num1 + num2;
+    return result;
+    function double(num) {
+        return num + 2;
+    }
+    const sum = double(20)
+    return sum;
+}
+
+const total = add(10, 20);
+console.log(total);
+// console.log(result);
