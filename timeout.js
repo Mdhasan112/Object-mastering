822d027a4ef2db80c0a66c281e5ca1f026aa7a18
hasan <mdhasanhk.98@gmail.com>
2022-07-29 14:01:00 -0400

    timeout

---
+++ timeout.js
@@ -0,0 +1,11 @@
+function doSomething() {
+    console.log(3333);
+}
+console.log(1111);
+console.log(2222);
+// setTimeout(doSomething, 2000);
+setInterval(function(){
+    console.log("let's do it",)
+}, 2000)
+console.log(4444);
+console.log(5555);
